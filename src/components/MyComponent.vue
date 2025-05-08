ef10b494a64b2c8a78e3da8fd50947c1eba007eb
Rangel <tylermz13@hotmail.com>
2025-05-08 15:32:40 -0600

    v-if, v-on, v-model

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div class="my-component">
+        <h1 v-if="isVisible" >{{message}}</h1>
+        <button v-on:click="sayHello">Aparecer - Desaparecer </button>
+        <input v-model="message">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MycomponentView',
+    data(){
+        return{
+            message: 'Hello World',
+            isVisible: true
+        }
+    },
+    methods: {
+        visibilidad(){
+            this.isVisible = !this.isVisible;
+        },
+        sayHello(){
+            alert('Hello!');
+        }
+    }
+}
+</script>
